28b59a6f58d3272950596ac957ba4c237ab737db
zhaoli <zhaoli@h2cominfo.com>
2018-12-25 19:07:24 +0800

    add

---
+++ src/views/service/viewOrder/myOrder.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="app-container">
+ <van-tabs v-model="active">
+  <van-tab title="全部">内容 1</van-tab>
+  <van-tab title="已下单">内容 2</van-tab>
+  <van-tab title="处理中">内容 3</van-tab>
+  <van-tab title="待评价">内容 4</van-tab>
+</van-tabs>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+       index: "我的订单"
+    }
+  },
+  components: {
+
+  }
+}
+</script>
+
+<style scoped>
+
+ 
+</style>
