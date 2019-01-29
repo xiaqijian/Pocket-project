2bbf7f69dcd24295674698dfedd57f1297a79da6
FreedomOn <15136616945@163.comm>
2019-01-29 15:56:00 +0800

    客户版新增订单详情页面

--- src/views/service/viewOrder/bussinessDetail.vue
+++ src/views/service/viewOrder/bussinessDetail.vue
@@ -1,37 +1,86 @@
 <template>
   <div class="app-container">
-   
-   </div>
+      <van-cell-group>
+        <van-cell title="工单编号" :value="datadetail.workOrderCode" />
+        <van-cell title="客户姓名" :value="datadetail.customerName" />
+        <van-cell title="客户电话" :value="datadetail.customerMobile" />
+        <van-cell title="客户地址" :value="datadetail.province + datadetail.area + datadetail.address">
+           <!-- <van-icon slot="right-icon" name="location" class="custom-icon" /> -->
+        </van-cell>
+        <van-cell title="工单业务" :value="datadetail.businessName" />
+        <van-cell title="工单类型" :value="datadetail.busDesc" />
+        <van-cell title="服务状态" :value="datadetail.statudDesc" />
+        <van-cell title="创建时间" :value="datadetail.addTime" />
+        <van-cell title="服务时间" :value="datadetail.modifierTime" />
+        
+      </van-cell-group>
+    <van-collapse v-model="activeNames1">
+      <van-collapse-item title="备注" name="1">
+         {{datadetail.remark}}
+      </van-collapse-item>
+    </van-collapse>
+
+      <van-collapse v-model="activeNames">
+        <van-collapse-item title="佣金说明" name="1">
+          <span v-for="item in datadetail.commissions" :key="item.id"> {{item.commissionName}} 一共{{item.score}}分 --- 每分 {{item.money}}元<br></span>
+        </van-collapse-item>
+      </van-collapse>
+      <div class="btn">
+        <van-button type="primary" size="large" @click.stop.prevent="callclick">电话联系</van-button>    
+      </div>
+
+  </div>
 </template>
 
 <script>
 export default {
   data () {
     return {
-      //查询id
-      workId:'',
-     
+       activeNames: ['1'],
+       activeNames1: ['1'],
+       workOrderId: 36,
+       datadetail: {},
+       message: '',
+       status: false
     }
   },
-  mounted(){
-  this.workId = this.$route.params.id
-  //获取详情数据
-  this.getOrderDetail();
+  mounted() {
+    this.workOrderId = this.$route.params.id
+    this.getdata(this.workOrderId)
   },
-  methods:{
-    getOrderDetail(){
-      this.$axios.get('pocket/wxchat/workOrderDetail',
-      {params:{
-        workOrderId:this.workId
-      }}).
-      then(res=>{
-    console.log(res)
-    this.data = res.data.data;
-      }).
-      catch(err=>{
-    this.$toast(err);
+  methods: {
+   getdata (id) {
+     let that = this;
+    this.$axios.get('pocket/wxchat/workOrderDetail', { params: { 'workOrderId': id }})
+      .then((res) => {
+        console.log(res.data.data)
+        that.datadetail = res.data.data
+         
+         if( that.datadetail.status == 1 ) {
+               that.status = true
+         } else if (that.datadetail.status == 2) {
+           that.status = true
+         } else {
+           that.status = false
+         }
+         console.log(that.status)
       })
-    }
+      .catch((err) => {
+        console.log(err)
+      })
+   },
+   callclick () {
+     let that = this
+      this.$dialog.confirm({
+          title: '提示',
+          message: '你即将通过电话联系客户，是否确认'
+        }).then(() => {
+          // on confirm
+           window.location.href = "tel:" + that.datadetail.customerMobile;
+        }).catch(() => {
+          // on cancel
+        });
+   },
   },
   components: {
 
@@ -39,8 +88,17 @@ export default {
 }
 </script>
 
-<style scoped>
+<style lang="less" type="text/less" scoped>
+.app-container {
+  // height: 100%;
+  background: #efefef;
+}
+.btn {
+  padding: 20px;
+  padding-bottom: 100px;
+  .van-button--large {
+     margin-top:30px;
+  }
+}
  
 </style>
-
-
